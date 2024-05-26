20bd82af6134a3a73adaec99cd4f4dfb46e757ef
RomainDWT <romain.collinmail@gmail.com>
2024-05-26 16:37:11 +0200

    section photoshop finit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,34 +1,29 @@
 "use strict";
-
+// SYSTEM TABS
 let links = document.querySelectorAll(".projet__linktab");
 
 for (let link of links) {
-    link.addEventListener('click', (event) => {
+    link.addEventListener('click', () =>{
         event.preventDefault();
 
-        if (link.classList.contains("projet__linktab--active")) {
+        if(link.classList.contains("projet__linktab--active")){
             clearAll();
             link.classList.remove("projet__linktab--active");
             let contentId = link.getAttribute("href");
             let content = document.querySelector(contentId);
-            content.classList.remove("projet__tabcontent--visible");
-            setTimeout(() => {
-                content.classList.add("projet__hidden");
-            }, 300); // Attendez la fin de la transition avant de cacher le contenu
-        } else {
-            clearAll();
+            content.classList.add("projet__hidden");
+
+        } else{
+            clearAll()
             link.classList.add("projet__linktab--active");
             let contentId = link.getAttribute("href");
             let content = document.querySelector(contentId);
             content.classList.remove("projet__hidden");
-            setTimeout(() => {
-                content.classList.add("projet__tabcontent--visible");
-            }, 10); // Attendez un bref délai avant d'ajouter la classe visible pour déclencher la transition
         }
-    });
+    })
 }
 
-function clearAll() {
+function clearAll(){
     let activeLinks = document.querySelectorAll(".projet__linktab");
     for (let activeLink of activeLinks) {
         activeLink.classList.remove("projet__linktab--active");
@@ -37,9 +32,9 @@ function clearAll() {
     let tabs = document.querySelectorAll(".projet__tabcontent");
     for (let tab of tabs) {
         tab.classList.add("projet__hidden");
-        tab.classList.remove("projet__tabcontent--visible");
     }
 }
+
 
 //Scroll horizontal
 const stickySections = [...document.querySelectorAll('.stage__sticky')];
@@ -59,6 +54,26 @@ function transform(section) {
     scrollSection.style.transform = `translate3d(${-percentage}vw, 0, 0)`;
 }
 
+// CODE POUR HOVER PHOTOS PROJETS
+document.addEventListener('DOMContentLoaded', function() {
+    let links = document.querySelectorAll(".projet__copia, .projet__readimade, .projet__helloworld, .projet__decembre, .projet__rux, .projet__dataplay");
+    let imageContainer = document.querySelector(".projet__image-container");
+    let image = document.querySelector(".projet__image");
 
-// GSAP
+    links.forEach(link => {
+        link.addEventListener('mouseover', () => {
+            let imageUrl = link.getAttribute("data-image");
+            image.src = imageUrl;
+            imageContainer.style.display = "block";
+        });
 
+        link.addEventListener('mouseout', () => {
+            imageContainer.style.display = "none";
+        });
+
+        link.addEventListener('mousemove', (event) => {
+            imageContainer.style.top = (event.pageY + 20) + 'px'; // Adjust position as needed
+            imageContainer.style.left = (event.pageX + 20) + 'px'; // Adjust position as needed
+        });
+    });
+});
